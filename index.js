108164636e95f7da9930df26a8dbc9500e6ea04a
Gabe L <gabe@fijiwebdesign.com>
2017-02-02 15:17:42 +0400

    Initial commit - Experimental!

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict'
+const electron = require('electron')
+const app = electron.app 
+const minimist = require('minimist')
+const argv = require('minimist')(process.argv.slice(2))
+
+// params
+const script_path = argv['path']
+
+console.log('Debugging ' + script_path)
+
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')({showDevTools: 'undocked'})
+
+// prevent window being garbage collected
+let mainWindow
+
+function onClosed() {
+	// dereference the window
+	// for multiple windows store them in an array
+	mainWindow = null
+}
+
+function createMainWindow() {
+	const win = new electron.BrowserWindow({
+		width: 600,
+		height: 400
+	})
+
+	win.loadURL(`file://${__dirname}/index.html?${script_path}`)
+	win.on('closed', onClosed)
+
+	//win.webContents.openDevTools();
+	win.maximize()
+
+	return win
+}
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if (!mainWindow) {
+		mainWindow = createMainWindow()
+	}
+})
+
+app.on('ready', () => {
+	mainWindow = createMainWindow()
+})
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Electron Scope
+
+> Node JS Debugger using Electron
+
+This is very experimental. 
+
+## Install
+
+Clone the electron-scope git repository.
+
+```
+$ git clone https://github.com/fijiwebdesign/electron-scope
+```
+
+Install the dependencies locally.
+```
+$ cd electron-scope
+$ npm install
+```
+
+### Run
+
+Run electron-scope with the local electron and pass it the script to debug in the --path option.
+
+```
+$ electron . --path /path/to/node/app.js
+```
+
+### Build
+
+```
+$ npm run build
+```
+
+Builds the app for OS X, Linux, and Windows, using [electron-packager](https://github.com/electron-userland/electron-packager).
+
+
+## License
+
+MIT © [Gabe](http://fijiwebdesign.com)
